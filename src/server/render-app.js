481052058e278dde7e3f61931335be57e05e22ec
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-04-15 15:59:32 +0200

    Started Chapter 05 - setting up Webpack manager.

--- src/server/render-app.js
+++ src/server/render-app.js
@@ -3,7 +3,8 @@
  */
 // @flow
 
-import { STATIC_PATH } from '../shared/config'
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
 
 const renderApp = (title: string) =>
   `<!doctype html>
@@ -13,7 +14,8 @@ const renderApp = (title: string) =>
     <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
   </head>
   <body>
-    <h1>${title}</h1>
+    <div class="${APP_CONTAINER_CLASS}"></div>
+    <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
   </body>
 </html>
 `
