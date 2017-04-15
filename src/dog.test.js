8fda29c276bfd89a5c76907ac6dbdcca6981989d
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-04-15 14:46:27 +0200

    Started Chapter 03 - adding a web server to our app.

--- src/dog.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Mihail on 4/15/2017.
- */
-import Dog from './dog'
-
-test('Dog.bark', () => {
-  const testDog = new Dog('Test')
-  expect(testDog.bark()).toBe('Wah wah, I am Test')
-})
