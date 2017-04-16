a744a7d1f820318c45364b23597ac14db554b0fc
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-04-16 16:34:54 +0200

    Added navigation (client side).

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -3,5 +3,9 @@
  */
 // @flow
 
-// eslint-disable-next-line import/prefer-default-export
+export const HOME_PAGE_ROUTE = '/'
+export const HELLO_PAGE_ROUTE = '/hello'
+export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async'
+export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404'
+
 export const helloEndpointRoute = (num: ?number) => `/ajax/hello/${num || ':num'}`
