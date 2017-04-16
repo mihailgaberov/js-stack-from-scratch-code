f9da3cdb924fb6d973d18fed659c7178eab26370
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-04-16 14:02:42 +0200

    Started Chapter 05. Introduces Redux and React-redux.

---
+++ src/client/action/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Mihail on 4/16/2017.
+ */
+// @flow
+
+import { createAction } from 'redux-actions'
+
+export const SAY_HELLO = 'SAY_HELLO'
+
+export const sayHello = createAction(SAY_HELLO)
